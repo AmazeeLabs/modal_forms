a5d304d5b81297e340f055f9020294eb9b6aa948
Fredrik Jonsson <frjo@xdeb.org>
2011-08-22 14:34:57 +0900

    Renamed modal styles to modal-popup-[style].

--- js/modal_forms_contact.js
+++ js/modal_forms_contact.js
@@ -4,7 +4,7 @@ Drupal.behaviors.initModalFormsContact = {
   attach: function (context, settings) {
     $("a[href*='/contact'], a[href*='?q=contact']", context).once('init-modal-forms-contact', function () {
       this.href = this.href.replace(/contact/,'modal_forms/nojs/contact');
-    }).addClass('ctools-use-modal ctools-modal-modal-forms-medium');
+    }).addClass('ctools-use-modal ctools-modal-modal-popup-medium');
   }
 };
 
